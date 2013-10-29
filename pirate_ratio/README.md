a228566242d4038cdd87334ff3de0cf77db48e68
Alex Bedley <aeoliant@hotmail.com>
2013-10-28 23:58:05 -0400

    added pirate_ratio shows seeder/leecher ratio

---
+++ pirate_ratio/README.md
@@ -0,0 +1,13 @@
+#Pirate Ratio
+
+#####Installation
+
+1. Download Zip (should be bottom right in this window) and unzip
+
+2. Navigate to [chrome://extensions/](chrome://extensions/)
+
+3. Drag pirate_ratio.crx from Finder to Chrome
+
+4. Follow the instructions to install
+
+5. Navigate to a pirate bay search link
---
+++ pirate_ratio/pirate_ratio/content_script.js
@@ -0,0 +1,26 @@
+console.log("Pirate Ratio loaded!");
+
+var round = function (num) {
+  var decimals = 4;
+  var sign = num >= 0 ? 1 : -1;
+  return (Math.round((num * Math.pow(10, decimals)) + (sign * 0.001)) / Math.pow(10, decimals)).toFixed(decimals);
+}
+
+$(".header").append("<th><abbr title='Ratio'><a href='#' title='Can't order by ratio yet!'>RT</a></abbr></th>");
+var seeders = [];
+var leechers = [];
+$("td:nth-child(3)").each(function(){
+  var seederCell = $(this);
+  seeders.push(parseInt(seederCell.html()));
+});
+$("td:nth-child(4)").each(function(){
+  var leecherCell = $(this);
+  leechers.push(parseInt(leecherCell.html()));
+});
+$("tr").not(".header").append(function(index, html) {
+  var row = $(this);
+  var numSeeders = seeders[index];
+  var numLeechers = leechers[index];
+  var ratio = round(numSeeders/numLeechers);
+  return "<td align='right'>" + ratio + "</td>";
+});
